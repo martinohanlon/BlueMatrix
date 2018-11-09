82309c20b6acba6853a7c06e7e81e4b619f98af5
Martin O'Hanlon <martin@ohanlonweb.com>
2018-11-09 16:47:48 +0000

    python debugging

---
+++ bluematrix/datatest.py
@@ -0,0 +1,19 @@
+from btcomm import BluetoothServer
+from time import sleep, time
+from signal import pause
+
+def data_received(data):
+    print("data received #{}#".format(data))
+    sleep(1)
+    s.send("hello")
+    sleep(1)
+    s.send("bye\n")
+
+def client_connects():
+    print("client connected")
+    sleep(1)
+    s.send("hi\nbye\n")
+
+s = BluetoothServer(data_received)
+s.when_client_connects = client_connects
+pause()
