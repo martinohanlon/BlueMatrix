c27c7a148300d6c989e2f475059caafa9edde5b5
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2018-11-18 22:42:08 +0000

    android tidy up

--- clients/android/app/src/main/java/com/stuffaboutcode/bluematrix/Constants.java
+++ clients/android/app/src/main/java/com/stuffaboutcode/bluematrix/Constants.java
@@ -32,4 +32,6 @@ public interface Constants {
     public static final String DEVICE_NAME = "device_name";
     public static final String TOAST = "toast";
 
+    public static final float BORDER_THICKNESS = (float)0.025;
+
 }
